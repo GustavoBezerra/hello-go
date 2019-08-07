b1f9bc08f6fae7619f03b33089c5c06af45405a1
Gustavo Bezerra <gustavobz305@gmail.com>
2019-08-06 21:33:27 -0300

    Criando funções

--- hello.go
+++ hello.go
@@ -1,30 +1,58 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
 	//var nome string = "Gustavo"
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não reconheço este comando")
+		os.Exit(-1)
+	}
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não reconheço este comando")
+	// }
+
+}
+
+func exibeIntroducao() {
 	nome := "Gustavo"
 	versao := 1.0
 	fmt.Println("Bem vindo", nome)
 	fmt.Println("Programa na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	var comando int
+func leComando() int {
+	var comandoLido int
 	//fmt.Scanf("%d", &comando)
-	fmt.Scan(&comando)
+	fmt.Scan(&comandoLido)
 
-	if comando == 1 {
-		fmt.Println("Monitorando...")
-	} else if comando == 2 {
-		fmt.Println("Exibindo Logs...")
-	} else if comando == 0 {
-		fmt.Println("Saindo do programa")
-	} else {
-		fmt.Println("Não reconheço este comando")
-	}
-
+	return comandoLido
 }
